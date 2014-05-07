23bb5a5b49db85c7bf205ec64a3c174fd10fb14a
Vangos Pterneas <vangos@lightbuzz.com>
2014-05-07 04:20:31 +0300

    Added stream recording and save as video

--- Kinect v2/WinRT/VitruviusTest/MainPage.xaml.cs
+++ Kinect v2/WinRT/VitruviusTest/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LightBuzz.Vitruvius;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using WindowsPreview.Kinect;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +24,171 @@ namespace VitruviusTest
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        VisualizationMode _mode = VisualizationMode.Color;
+
+        KinectSensor _sensor;
+        MultiSourceFrameReader _reader;
+        IEnumerable<Body> _bodies;
+        GestureController _gestureController;
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            _sensor = KinectSensor.GetDefault();
+
+            if (_sensor != null)
+            {
+                _sensor.Open();
+
+                _reader = _sensor.OpenMultiSourceFrameReader(FrameSourceTypes.Color | FrameSourceTypes.Depth | FrameSourceTypes.Infrared | FrameSourceTypes.Body);
+                _reader.MultiSourceFrameArrived += Reader_MultiSourceFrameArrived;
+
+                _gestureController = new GestureController(GestureType.All);
+                _gestureController.GestureRecognized += GestureController_GestureRecognized;
+            }
+        }
+
+        private void Page_Unloaded(object sender, RoutedEventArgs e)
+        {
+            if (_reader != null)
+            {
+                _reader.Dispose();
+            }
+
+            if (_bodies != null)
+            {
+                if (_bodies.Count() > 0)
+                {
+                    foreach (var body in _bodies)
+                    {
+                        body.Dispose();
+                    }
+                }
+            }
+
+            if (_sensor != null)
+            {
+                _sensor.Close();
+            }
+        }
+
+        void Reader_MultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs e)
+        {
+            var reference = e.FrameReference.AcquireFrame();
+
+            // Color
+            using (var frame = reference.ColorFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    if (_mode == VisualizationMode.Color)
+                    {
+                        camera.Source = frame.ToBitmap();
+                    }
+                }
+            }
+
+            // Depth
+            using (var frame = reference.DepthFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    if (_mode == VisualizationMode.Depth)
+                    {
+                        camera.Source = frame.ToBitmap();
+                    }
+                }
+            }
+
+            // Infrared
+            using (var frame = reference.InfraredFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    if (_mode == VisualizationMode.Infrared)
+                    {
+                        camera.Source = frame.ToBitmap();
+                    }
+                }
+            }
+
+            // Body
+            using (var frame = reference.BodyFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    //tblHeights.Text = string.Empty;
+
+                    _bodies = frame.Bodies().Where(body => body.IsTracked);
+
+                    foreach (var body in _bodies)
+                    {
+                        if (body.IsTracked)
+                        {
+                            // Update body gestures.
+                            _gestureController.Update(body);
+
+                            // Draw body.
+                            //canvas.Source = body.ToBitmap(_mode);
+
+                            // Display user height.
+                            //tblHeights.Text += string.Format("\nUser {0}: {1}cm", body.TrackingId, Math.Round(body.Height(), 2));
+                        }
+                    }
+                }
+            }
+        }
+
+        void GestureController_GestureRecognized(object sender, GestureEventArgs e)
+        {
+            // Display the gesture type.
+            //tblGestures.Text = e.Name;
+
+            // Do something according to the type of the gesture.
+            switch (e.Type)
+            {
+                case GestureType.JoinedHands:
+                    break;
+                case GestureType.Menu:
+                    break;
+                case GestureType.SwipeDown:
+                    break;
+                case GestureType.SwipeLeft:
+                    break;
+                case GestureType.SwipeRight:
+                    break;
+                case GestureType.SwipeUp:
+                    break;
+                case GestureType.WaveLeft:
+                    break;
+                case GestureType.WaveRight:
+                    break;
+                case GestureType.ZoomIn:
+                    break;
+                case GestureType.ZoomOut:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Color_Click(object sender, RoutedEventArgs e)
+        {
+            _mode = VisualizationMode.Color;
+        }
+
+        private void Depth_Click(object sender, RoutedEventArgs e)
+        {
+            _mode = VisualizationMode.Depth;
+        }
+
+        private void Infrared_Click(object sender, RoutedEventArgs e)
+        {
+            _mode = VisualizationMode.Infrared;
         }
     }
 }
