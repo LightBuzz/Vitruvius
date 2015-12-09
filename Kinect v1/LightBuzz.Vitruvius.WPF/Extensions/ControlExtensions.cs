ba50c9c6d64dc4cc1ed26ef9a67be8066b2d8949
George Birbilis <GitHub1@zoomicon.com>
2015-12-09 14:19:15 +0200

    - Added Class Diagrams (.cd) - Changed ControlExtensions.cs in Kinect v1 WPF project to have .WPF suffix in its namespace for consistency [breaking change] - At Vitruvius.WPF and Vitruvius.WinForms projects, extracted DepthImageMode class from inside DepthExtensions.cs and placed at Vitruvius base project in Core subfolder (merged WPF and WinForms versions by moving to removing WPF and WinForms suffixes from namespace) [breaking change] - Added Extensions subfolder to base project and moved its Extensions classes there - Added Core subrolder to base project and moved VisualizationMode.cs there (to be together with the DepthImageMode.cs that was merged and brought in from the WPF/WinForms projects) - Added subfolder Controllers to base project and moved VoiceController.cs there - Added NamespaceDoc.cs files where missing to document namespaces - Added reference to base project from the WinForms one, since DepthImageMode class has moved to the latter

--- Kinect v1/LightBuzz.Vitruvius.WPF/Extensions/ControlExtensions.cs
+++ Kinect v1/LightBuzz.Vitruvius.WPF/Extensions/ControlExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 
-namespace LightBuzz.Vitruvius
+namespace LightBuzz.Vitruvius.WPF
 {
     public static class ControlExtensions
     {
--- Kinect v1/LightBuzz.Vitruvius.WinForms/DepthExtensions.cs
+++ Kinect v1/LightBuzz.Vitruvius.WinForms/DepthExtensions.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Kinect;
+﻿using LightBuzz.Vitruvius;
+
+using Microsoft.Kinect;
+
 using System;
 using System.Drawing;
 using System.Drawing.Imaging;
@@ -287,29 +290,4 @@ namespace LightBuzz.Vitruvius.WinForms
         #endregion
     }
 
-    /// <summary>
-    /// Represents the depth image mode (raw pixels, grayscale, colored).
-    /// </summary>
-    public enum DepthImageMode
-    {
-        /// <summary>
-        /// The simplest representation of a depth image.
-        /// </summary>
-        Raw,
-
-        /// <summary>
-        /// Depth image representation in a grayscale format.
-        /// </summary>
-        Dark,
-
-        /// <summary>
-        /// Colored depth image representation.
-        /// </summary>
-        Colors,
-
-        /// <summary>
-        /// Player depth frame representation.
-        /// </summary>
-        Player
-    }
 }
